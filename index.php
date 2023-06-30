19b10cfb36a3144e81f9f26e2960ced6a4b4b2e0
FRAALES <alessandrofranco1992@gmail.com>
2023-06-30 14:16:56 +0200

    Aggiunto il genere

--- index.php
+++ index.php
@@ -4,21 +4,23 @@ class Movie {
     public $title;
     public $director;
     public $mainActor;
+    public $genre;
 
-    public function __construct ($title, $director, $mainActor) {
+    public function __construct ($title, $director, $mainActor, $genre) {
         $this -> title = $title;
         $this -> director = $director;
         $this -> mainActor = $mainActor;
+        $this -> genre = $genre;
     }
 
     public function getFullMovie () {
-        return $this -> title . "," . $this -> director . "," . $this -> mainActor;
+        return $this -> title . "," . $this -> director . "," . $this -> mainActor . "," . $this -> genre;
     }
 }
 
 
-$movie1 = new Movie ("Ritorno al Futuro", "Robert Zemeckis", "Christopher Lloyd");
-$movie2 = new Movie ("The Lord of the Rings", "Peter Jackson", "Elijah Wood");
+$movie1 = new Movie ("Back to the Future", "Robert Zemeckis", "Christopher Lloyd", "Action");
+$movie2 = new Movie ("The Lord of the Rings", "Peter Jackson", "Elijah Wood", "Fantasy");
 
 
 
