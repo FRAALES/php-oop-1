68c7c0a13a5c9f485c25049af4c3285173f09802
FRAALES <alessandrofranco1992@gmail.com>
2023-06-30 16:02:19 +0200

    Bonus 1 fatto

--- index.php
+++ index.php
@@ -1,26 +1,59 @@
 <?php
 
-class Movie {
-    public $title;
-    public $director;
-    public $mainActor;
-    public $genre;
+class Genre {
 
-    public function __construct ($title, $director, $mainActor, $genre) {
-        $this -> title = $title;
-        $this -> director = $director;
-        $this -> mainActor = $mainActor;
-        $this -> genre = $genre;
-    }
+    public $scene;
+    public $music;
+    public $setting;
 
-    public function getFullMovie () {
-        return $this -> title . "," . $this -> director . "," . $this -> mainActor . "," . $this -> genre;
+    public function __construct($scene, $music, $setting) {
+
+        $this -> scene = $scene;
+        $this -> music = $music;
+        $this -> setting = $setting;
     }
 }
 
+class Movie {
 
-$movie1 = new Movie ("Back to the Future", "Robert Zemeckis", "Christopher Lloyd", "Action");
-$movie2 = new Movie ("The Lord of the Rings", "Peter Jackson", "Elijah Wood", "Fantasy");
+    public static $movieCounter = 0;
+
+    public $title;
+    public $director;
+    public $mainActor;
+
+
+    public Array $genres;
+
+    public function __construct ($title, $director, $mainActor, genre ...$genres) {
+
+        self :: $movieCounter++;
+
+        $this -> title = $title;
+        $this -> director = $director;
+        $this -> mainActor = $mainActor;
+
+        $this -> genres = $genres;
+    }
+
+    public function getFullMovie () {
+        return $this -> title . "," . $this -> director . "," . $this -> mainActor;
+    }
+
+    public function getGenere(){
+        return $this -> genre -> genere;
+    }
+
+    public static function getCounter() {
+        return self :: $movieCounter;
+    }
+}
+
+$genre1 = new Genre("Action", "actionmusic", "actionsetting");
+$genre2 = new Genre("Fantasy", "fantasymusic", "fantasysetting");
+
+$movie1 = new Movie ("Back to the Future", "Robert Zemeckis", "Christopher Lloyd", $genre1);
+$movie2 = new Movie ("The Lord of the Rings", "Peter Jackson", "Elijah Wood", $genre1, $genre2);
 
 
 
